c4c5daefb2e5d0c8ea7e8801afca394ad2c43d4a
SystemAdminTkinter <gancster007@gmail.com>
2026-02-27 11:19:47 +0500

    ...

--- Models/User.py
+++ Models/User.py
@@ -4,4 +4,10 @@ class User(BaseModel):
     id = PrimaryKeyField()
     login = CharField(unique=True, max_length=10)
     password = CharField(max_length=255)
-    role = CharField()
+    role = CharField(choices=[
+        'Пользователь',
+        'Администратор',
+        'Специалист техподдержки'
+    ])
+    is_active = BooleanField(default=True) # Альтернатива удаления
+    fullname = CharField(null=True, max_length=150)
