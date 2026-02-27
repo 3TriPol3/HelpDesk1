140b881c233cee20af681904b96452eb1ed57fff
SystemAdminTkinter <gancster007@gmail.com>
2026-02-27 10:44:30 +0500

    ...

---
+++ Connection/connect.py
@@ -0,0 +1,10 @@
+from peewee import *
+
+mysql_db = MySQLDatabase('BessM82_HelpDesk',
+                         user='BessM82_HelpDesk',
+                         password='111111',
+                         host='10.11.13.118',
+                         port=3306)
+if __name__ == "__main__":
+    print(mysql_db.connect())
+
